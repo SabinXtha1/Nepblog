5db0b4e5409d247f5bb7d86dda475e130ceca7d1
Sabin Nayaju <raj123nayaju@gmail.com>
2025-05-22 23:14:57 +0545

    add middleware to protect route

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,24 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+const isAdminRoute = createRouteMatcher(['/admin/:path*']);
+
+export default clerkMiddleware((auth, request) => {
+
+  if (isAdminRoute(request)) {
+
+    if (!auth().userId) {
+   
+      return NextResponse.redirect(new URL('/sign-in', request.url));
+    }
+  
+  }
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
+    '/admin/:path*',
   ],
 };
